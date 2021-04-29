2fa9f140f3d15447d7bcaa41e53d2a9b3526a1b8
Justin Smith <smith.justinmatthew@gmail.com>
2021-04-29 19:56:10 -0400

    Update ok_wake.py
    Updated functionality.

--- ok_wake.py
+++ ok_wake.py
@@ -1,45 +1,121 @@
-# Neopixel OK to Wake clock
+#!/usr/bin/env python3
 
 import time
-from datetime import datetime, timedelta
-import sched
-
+import datetime as dt
 from neopixel import *
+import argparse
+import atexit
+
+# LED strip configuration:
+LED_COUNT      = 12      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 20     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
-# LED ring configuration
-LED_COUNT = 12  # Number of LED pixels.
-LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 5  # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 25  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 
-
-# Define functions which animate LEDs for ok-to-wake clock
-def stay_in_bed(strip, color, wait_ms=50):
-    """Red color from bedtime until time to wake"""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color=(205, 92, 92))
+# Define functions which animate LEDs in various ways.
+def sleepTime(strip, color, wait_ms=50):
+    """Solid color, full pixels"""
+    for n in range(0, strip.numPixels()):
+        strip.setPixelColor(n, color)
         strip.show()
+        time.sleep(wait_ms/1000.0)
 
 
-def up_time(strip, color, wait_ms=50):
-    """Green color from wake_up time until 30 minutes has passed"""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color=(60, 179, 113))
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    strip.setBrightness(2)
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
         strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+
+def lightOff(strip, color):
+    """Light turns off, color wipe"""
+    n = int(strip.numPixels())
+    strip.setPixelColor(n, color)
+    strip.show()
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def lightOff(strip, color):
+    """Light turns off, color wipe"""
+    n = int(strip.numPixels())
+    strip.setPixelColor(n, color)
+    strip.show()
 
 
-def light_off(strip, color):
-    """The light is off"""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color=(0, 0, 0))
-        strip.show()
-
-
+# Main program logic follows:
 if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
     # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-    # Initialize the library (must be called once before other functions).
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
     strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+
+        while True:
+            if dt.datetime.now().hour <= 6 or dt.datetime.now().hour >= 19:
+                sleepTime(strip, Color(0, 0, 0))  # Red  Pixels
+            elif dt.datetime.now().hour == 7:
+                sleepTime(strip, Color(255, 0, 0))  # Green Pixels
+            else:
+                lightOff(strip, Color(0, 0, 0))
+
+
+    except KeyboardInterrupt:
+        if args.clear:
+            print('args cleared, light off')
+            lightOff(strip, Color(0, 0, 0))
